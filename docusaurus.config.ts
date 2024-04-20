5dd82c2682af93ee5b916fc1d0a28b8ba09a1138
Laban Eilers <laban@eilers.com>
2024-04-20 11:54:40 -0400

    fix base url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://labaneilers.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/labaneilers.com/',
 
   trailingSlash: false,
 
